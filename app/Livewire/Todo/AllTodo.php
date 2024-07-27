34b18604a4cdf8ab6239cc681353a302e3418088
Your Name <naimmd775@gmail.com>
2024-07-27 11:08:43 +0600

    update

--- app/Livewire/Todo/AllTodo.php
+++ app/Livewire/Todo/AllTodo.php
@@ -5,14 +5,18 @@ namespace App\Livewire\Todo;
 use App\Models\Todo;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AllTodo extends Component
 {
+    use WithPagination;
+
+    public $search;
     #[On('todo-created')]
     public function render()
     {
         return view('livewire.todo.all-todo',[
-            'todos' => Todo::latest()->get()
+            'todos' => Todo::where('todo','like',"%{$this->search}%")->latest()->paginate(5)
         ]);
     }
 }
--- resources/views/livewire/todo/all-todo.blade.php
+++ resources/views/livewire/todo/all-todo.blade.php
@@ -15,8 +15,7 @@
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="todo-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 "  />
-                    <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
+                    <input type="text" wire:model.live.debounce.500ms="search" id="todo-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 "  />
                 </div>
             </form>
 
@@ -48,6 +47,9 @@
                         @endforeach
                     </tbody>
                 </table>
+                <div class="pt-5">
+                    {{ $todos->links(data: ['scrollTo' => false]) }}
+                </div>
             </div>
 
             </div>
